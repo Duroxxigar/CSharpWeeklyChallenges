1db802b6615e8dea0b012e6f51b689b7ba051908
Duroxxigar <young9003@gmail.com>
2019-05-05 13:25:31 -0400

    Created solution and week one project

---
+++ WeekOne-foreach/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekOne_foreach
+{
+    class Program
+    {
+        /*
+         *  Create a list of strings, putting in a few names. Use a foreach loop to print those to the console
+         *  
+         *  **BONUS**
+         *  Create another list of models. Models should store 2 string values. foreach loop should print out both values for each item.
+         * 
+         */
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
